d3286b360c22f66fc99bc728d22a9071e08ed524
Nathi Taengnim <>
2022-02-25 00:30:02 +0000

    quick scoper trait

--- api/app/Models/Product.php
+++ api/app/Models/Product.php
@@ -2,28 +2,19 @@
 
 namespace App\Models;
 
-use App\Scoping\Scoper;
 use App\Models\Category;
-use Illuminate\Database\Eloquent\Builder;
+use App\Models\Traits\CanBeScroped;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, CanBeScroped;
 
     public function getRouteKeyName()
     {
         return 'slug';
     }
-
-    public function scopeWithScopes(Builder $builder, $scopes = [])
-    {
-        
-        return (new Scoper(request()))->apply($builder, $scopes);
-
-    }
-
 
     public function categories()
     {
