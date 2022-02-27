1f1b67762c264ed512ec2f45562815b3c2b44341
Nathi Taengnim <>
2022-02-27 23:25:28 +0000

    select product variation on client side

--- client/components/products/ProductVariation.vue
+++ client/components/products/ProductVariation.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="bg-gray-500 p-3 mt-5">
         <label for="">{{ type }}</label>
+      
         <div>
             <div>
-                <select name="" id="">
+                <select :value="selectedVariationID" @change="change($event, type)">
                     <option value="">Please choose</option>
                     <option v-for="variation in variations" 
                     :key="variation.id"
@@ -32,10 +33,35 @@ export default {
         variations: {
             trequired: true,
             type: Array
+        },
+        value: {
+            required: false,
+            default: ''
         }
     },
-    setup() {
-        
+    
+    computed: {
+        selectedVariationID () {
+            if(!this.findVariation(this.value.id)) {
+                return ''
+            }
+            return this.value.id
+        }
     },
+
+    methods:{
+        change (event, type) {
+            this.$emit('input', this.findVariation(event.target.value))
+        },
+        findVariation (id) {
+            let variation = this.variations.find( v => v.id == id)
+
+            if(typeof variation === 'undefined') {
+                return null
+            }
+
+            return variation
+        }
+    } 
 }
 </script>
--- client/pages/products/_slug.vue
+++ client/pages/products/_slug.vue
@@ -21,7 +21,30 @@
                             :type="type"
                             :key="type"
                             :variations="variations"
+                            v-model="form.variation"
                             />
+                            
+                            <template v-if="form.variation" >
+                                <div class="custom-number-input h-10 w-32">
+                               
+                                    <div class="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
+                                        <button data-action="decrement" class=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
+                                        <span class="m-auto text-2xl font-thin">−</span>
+                                        </button>
+                                        <input type="number" class="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none" name="custom-input-number" value="0"></input>
+                                        <button data-action="increment" class="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
+                                            <span class="m-auto text-2xl font-thin">+</span>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div class="flex item-center justify-between mt-3">
+                                    <button
+                                        class="px-3 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded"
+                                    >
+                                        Add to Card
+                                    </button>
+                                </div>
+                            </template>
                         </form>
                     </section>
                 </div>
@@ -34,7 +57,11 @@
     export default {
         data () {
             return { 
-                product: null
+                product: null,
+                form: {
+                    variation: '',
+                    quantity: 1
+                }
             }
         },
         async asyncData ({ params, app }) {
@@ -46,3 +73,19 @@
         }
     }
 </script>
+
+<style>
+  input[type='number']::-webkit-inner-spin-button,
+  input[type='number']::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  .custom-number-input input:focus {
+    outline: none !important;
+  }
+
+  .custom-number-input button:focus {
+    outline: none !important;
+  }
+</style>
