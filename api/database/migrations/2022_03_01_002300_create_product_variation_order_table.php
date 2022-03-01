22ad01843188dcbd545e5fe2d07dbf2c94861da6
Nathi Taengnim <>
2022-03-01 00:29:40 +0000

    basic order

---
+++ api/database/migrations/2022_03_01_002300_create_product_variation_order_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductVariationOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_variation_order', function (Blueprint $table) {
+            $table->bigInteger('order_id')->unsigned()->index();
+            $table->bigInteger('product_variation_id')->unsigned()->index();
+            $table->integer('quantity')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('product_variation_id')->references('id')->on('product_variations');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_variation_order');
+    }
+}
